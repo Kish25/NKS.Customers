749322a2985f96284590b017001a22b500ce2321
Kishor Nagare <Kishor@villaplus.com>
2021-07-01 22:54:21 +0100

    database update

--- NKS.Customers.SQLDatabase/Tables/Addresses.sql
+++ NKS.Customers.SQLDatabase/Tables/Addresses.sql
@@ -4,18 +4,13 @@ CREATE TABLE Dbo.Addresses
 	Id				UniqueIdentifier NOT NULL ,
 	CustomerId		UniqueIdentifier NOT NULL ,
 	IsCurrent		Bit NOT NULL default(0),
-	HouseNumber		varchar(10) NOT NULL default(0),
-	Property		varchar(75) NULL ,
-	StreetName		varchar(75) NOT NULL ,
-	Landmark		varchar(75) NULL ,
+	Address1		varchar(50) NOT NULL default(0),
+	Address2		varchar(50) NULL ,
+	Address3		varchar(50) NOT NULL ,
 	Town			varchar(50) NULL ,
 	County			varchar(50) NOT NULL,
 	Country			varchar(50) NOT NULL,
 	Postcode		varchar(15) NOT NULL default(''),
-	Accuracy		 varchar(10) NULL,
-	ActionedByUserId	UniqueIdentifier NULL ,
-    EffectiveFrom	datetime2 NOT NULL DEFAULT SYSUTCDATETIME(),
-    EffectiveUntil	datetime2  NOT NULL DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.99999999'),
 	CONSTRAINT PK_Addresses  PRIMARY KEY (Id)
 )
 GO
@@ -24,5 +19,4 @@ CREATE INDEX IX_Addresses_CustomerId ON Dbo.Addresses (CustomerId)
 GO
 CREATE INDEX IX_Addresses_Postcode ON Dbo.Addresses (PostCode)
 GO
-CREATE INDEX IX_Addresses_HouseNumber ON Dbo.Addresses (HouseNumber)
-GO
+CREATE INDEX IX_Addresses_HouseNumber ON Dbo.Addresses (Address1)
--- NKS.Customers.SQLDatabase/Tables/Customers.sql
+++ NKS.Customers.SQLDatabase/Tables/Customers.sql
@@ -1,10 +1,14 @@
-﻿CREATE TABLE Dbo.Customers
+﻿
+CREATE TABLE dbo.Customers
 (
-	Id bigint Identity(1,1) NOT NULL,
-    TitleID Int NOT NULL Default(0), 
+	Id UniqueIdentifier  NOT NULL,
+    Title varchar(10) NOT NULL Default(''), 
     Forename nVarchar(50) NOT NULL , 
-    Initials nVarchar(10) NOT NULL Default('') , 
+	Initials nVarchar(10) NOT NULL , 
     Surname nVarchar(50) NOT NULL , 
+    Email nVarchar(100) NOT NULL , 
+    HashedEmail nvarchar(300) NOT NULL , 
+    MobileNumber Varchar(20) NOT NULL , 
     DateofBirth Date NULL, 
 	Status smallint NOT NULL Default(0) ,			-- 0-: Inactive, 1- Active 2-Block, 4-Inactive 8-No Comms, 16- BadReviews
 	EffectiveFrom datetime2 NOT NULL DEFAULT SYSUTCDATETIME(),	-- Create Default and bind for easy reference
@@ -13,7 +17,7 @@
 )
 GO
 
-CREATE INDEX IX_Customers_Title ON Dbo.Customers (TitleID)
-GO
 CREATE INDEX IX_Customers_Forename ON Dbo.Customers (Forename)
+GO
+ALTER TABLE Dbo.Customers ADD PERIOD FOR SYSTEM_TIME (EffectiveFrom,EffectiveUntil);
 GO
